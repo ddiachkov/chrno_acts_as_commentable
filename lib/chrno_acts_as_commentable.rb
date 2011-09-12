5b97ee55e44c4d21138af7a2f40b62683a8dc1f1
Denis Diachkov <d.diachkov@gmail.com>
2011-09-12 23:31:49 +0400

    Shut up.

--- lib/chrno_acts_as_commentable.rb
+++ lib/chrno_acts_as_commentable.rb
@@ -5,6 +5,14 @@ module ActsAsCommentable
   autoload :ARExtension, "acts_as_commentable/ar_extension"
   autoload :CommentBase, "acts_as_commentable/comment_base"
   autoload :VERSION,     "acts_as_commentable/version"
-end
 
-require "acts_as_commentable/engine"
+  class Engine < Rails::Engine
+    initializer "acts_as_commentable.initialization" do
+      # Загрузка в AR
+      ActiveSupport.on_load( :active_record ) do
+        Rails.logger.debug "--> load acts_as_commentable"
+        extend ActsAsCommentable::ARExtension
+      end
+    end
+  end
+end
